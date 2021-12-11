4c5afcb31222805ec92e3e8ee650742fa2df5041
Quistar <simon.ueng@outlook.com>
2021-12-12 00:33:51 +0800

    Prop Painter, Prop Precision Prop Snapping legacy loader

---
+++ LegacyDataHanlders/PropPainter/PropPainterDataContainer.cs
@@ -0,0 +1,26 @@
+﻿using ColossalFramework.IO;
+using EManagersLib;
+using UnityEngine;
+
+namespace PropPainter {
+    public class PropPainterDataContainer : IDataContainer {
+        // This reads the object (from bytes)
+        public void Deserialize(DataSerializer s) {
+            EPropInstance[] props = EPropManager.m_props.m_buffer;
+            int[] ids = s.ReadInt32Array();
+            for (int i = 0; i < ids.Length; i++) {
+                int h = ids[i];
+                if (h != 16777216) {
+                    byte r = (byte)((h >> 16) & 0xff);
+                    byte g = (byte)((h >> 8) & 0xff);
+                    byte b = (byte)((h) & 0xff);
+                    props[i].m_color = new Color32(r, g, b, 255);
+                }
+            }
+        }
+
+        public void AfterDeserialize(DataSerializer s) { }
+
+        public void Serialize(DataSerializer s) { }
+    }
+}
---
+++ LegacyDataHanlders/PropPrecision/Data.cs
@@ -0,0 +1,28 @@
+﻿using ColossalFramework.IO;
+using EManagersLib;
+
+/* Special class to load old Prop Precision data
+ */
+namespace PropPrecision {
+    public class Data : IDataContainer {
+        public void Serialize(DataSerializer s) { }
+
+        public void Deserialize(DataSerializer s) {
+            EPropInstance[] props = EPropManager.m_props.m_buffer;
+            var arraySize = s.ReadInt32();
+            for (int i = 0; i < arraySize; i++) {
+                uint propID = s.ReadUInt16();
+                float preciseX = s.ReadUInt16();
+                float preciseZ = s.ReadUInt16();
+                short posX = props[propID].m_posX;
+                short posZ = props[propID].m_posZ;
+                preciseX = (posX > 0 ? posX + preciseX / ushort.MaxValue : posX - preciseX / ushort.MaxValue) * 0.263671875f;
+                preciseZ = (posZ > 0 ? posZ + preciseZ / ushort.MaxValue : posZ - preciseZ / ushort.MaxValue) * 0.263671875f;
+                props[propID].m_preciseX = preciseX * 3.79259253f - posX;
+                props[propID].m_preciseZ = preciseZ * 3.79259253f - posZ;
+            }
+        }
+
+        public void AfterDeserialize(DataSerializer s) { }
+    }
+}
