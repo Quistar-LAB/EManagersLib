56955e42db53be10d29a9795530ea7bd22995d9a
Quistar <simon.ueng@outlook.com>
2021-12-30 19:01:18 +0800

    81 tiles framework

--- EDistrictManager.cs
+++ EDistrictManager.cs
@@ -1,7 +1,55 @@
-﻿using UnityEngine;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
 
 namespace EManagersLib {
     public static class EDistrictManager {
+        public const int DEFAULTGRID_RESULTION = 512;
+        public const float DISTRICTGRID_CELL_SIZE = 19.2f;
+        public const int DISTRICTGRID_RESOLUTION = 900;
+        public const int MAX_DISTRICT_COUNT = 128;
+        public const float CELL_AREA_TO_SQUARE = 5.76f;
+
+        private static int ID_DistrictsA1;
+        private static int ID_DistrictsA2;
+        private static int ID_DistrictsB1;
+        private static int ID_DistrictsB2;
+        private static int ID_EdgeColorA;
+        private static int ID_AreaColorA;
+        private static int ID_EdgeColorB;
+        private static int ID_AreaColorB;
+        private static int ID_ColorR;
+        private static int ID_ColorG;
+        private static int ID_ColorB;
+        private static int ID_ColorA;
+        private static int ID_ColorRG;
+        private static int ID_DistrictMapping;
+        private static int ID_Highlight1;
+        private static int ID_Highlight2;
+
+        public static void Awake(DistrictManager districtManager) {
+            districtManager.m_districtGrid = new DistrictManager.Cell[DISTRICTGRID_RESOLUTION * DISTRICTGRID_RESOLUTION];
+            districtManager.m_parkGrid = new DistrictManager.Cell[DISTRICTGRID_RESOLUTION * DISTRICTGRID_RESOLUTION];
+            //districtManager.m_color
+            ID_DistrictsA1 = Shader.PropertyToID("_DistrictsA1");
+            ID_DistrictsA2 = Shader.PropertyToID("_DistrictsA2");
+            ID_DistrictsB1 = Shader.PropertyToID("_DistrictsB1");
+            ID_DistrictsB2 = Shader.PropertyToID("_DistrictsB2");
+            ID_DistrictMapping = Shader.PropertyToID("_DistrictMapping");
+            ID_Highlight1 = Shader.PropertyToID("_Highlight1");
+            ID_Highlight2 = Shader.PropertyToID("_Highlight2");
+            ID_EdgeColorA = Shader.PropertyToID("_EdgeColorA");
+            ID_AreaColorA = Shader.PropertyToID("_AreaColorA");
+            ID_EdgeColorB = Shader.PropertyToID("_EdgeColorB");
+            ID_AreaColorB = Shader.PropertyToID("_AreaColorB");
+            ID_ColorR = Shader.PropertyToID("_ColorR");
+            ID_ColorG = Shader.PropertyToID("_ColorG");
+            ID_ColorB = Shader.PropertyToID("_ColorB");
+            ID_ColorA = Shader.PropertyToID("_ColorA");
+            ID_ColorRG = Shader.PropertyToID("_ColorRG");
+        }
+
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void MoveParkProps(DistrictPark[] parks, int cellX, int cellZ, byte src, byte dest) {
             int startX = EMath.Max((int)((cellX - 256f) * (19.2f / 64f) + 135f), 0);
             int startZ = EMath.Max((int)((cellZ - 256f) * (19.2f / 64f) + 135f), 0);
---
+++ EGameAreaManager.cs
@@ -0,0 +1,58 @@
+﻿using ColossalFramework;
+using ColossalFramework.IO;
+using EManagersLib.LegacyDataHandlers.EightyOneTiles;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace EManagersLib {
+    public static class EGameAreaManager {
+        private const string EIGHTYONE_KEY = "fakeGAM";
+        private const uint SaveFormatVersion = 1;
+        internal const int DEFAULTRESOLUTION = 1920;
+        internal const int DEFAULTAREACOUNT = 25;
+        internal const int DEFAULTGRIDSIZE = 5;
+        internal const int MINAREACOUNT = 9;
+        internal const int CUSTOMAREACOUNT = 81;
+        internal const int CUSTOMGRIDSIZE = 9;
+        internal const int CUSTOMAREATEXSIZE = 10;
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void GetTileXZ(GameAreaManager _, int tile, out int x, out int z) {
+            x = tile % CUSTOMGRIDSIZE;
+            z = tile / CUSTOMGRIDSIZE;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static int GetTileIndex(GameAreaManager _, int x, int z) => z * CUSTOMGRIDSIZE + x;
+
+        private static Type EightyOneDataLegacyHandler(string _) => typeof(EightyOneDataContainer);
+
+        internal static void IntegratedDeserialize() {
+            try {
+                if (Singleton<SimulationManager>.instance.m_serializableDataStorage.TryGetValue(EIGHTYONE_KEY, out byte[] data)) {
+                    EUtils.ELog("Found 81 Tiles data, loading...");
+                    using (MemoryStream stream = new MemoryStream(data)) {
+                        DataSerializer.Deserialize<EightyOneDataContainer>(stream, DataSerializer.Mode.Memory, EightyOneDataLegacyHandler);
+                    }
+                    EUtils.ELog("Loaded " + (data.Length / 1024) + "kb of 81 Tiles data");
+                } else {
+                    EUtils.ELog("No 81 Tiles data found");
+                }
+            } catch (Exception e) {
+                UnityEngine.Debug.LogException(e);
+            }
+        }
+
+        internal static void Serialize() {
+            byte[] data;
+            using (var stream = new MemoryStream()) {
+                DataSerializer.Serialize(stream, DataSerializer.Mode.Memory, SaveFormatVersion, new EightyOneDataContainer());
+                data = stream.ToArray();
+            }
+            ESerializableData.SaveData(EIGHTYONE_KEY, data);
+            EUtils.ELog($"Saved {data.Length} bytes of 81 Tiles data");
+        }
+
+    }
+}
