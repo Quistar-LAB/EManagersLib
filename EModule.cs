2cae02a80e849d7b67128418ab0237f3e4f01e42
Quistar <simon.ueng@outlook.com>
2022-01-24 08:31:16 +0800

    Version bump

--- EModule.cs
+++ EModule.cs
@@ -9,15 +9,12 @@ using System.IO;
 
 namespace EManagersLib {
     public sealed class EModule : IUserMod, ILoadingExtension {
-        internal const string m_modVersion = "1.0.8";
+        internal const string m_modVersion = "1.0.9";
         internal const string m_assemblyVersion = m_modVersion + ".*";
         internal const string m_modName = "Extended Managers";
         internal const string m_modDesc = "A library that extends the existing framework in Cities Skylines";
         internal const string HARMONYID = "quistar.EManagersLib.mod";
         internal const string m_settingsFile = "EManagersLibKeyBind";
-#if EMERGENCYRESCUE
-        private const string ExportedData = "EML_ExportedData.dat";
-#endif
 
         public string Name => m_modName + ' ' + m_modVersion;
         public string Description => m_modDesc;
@@ -40,6 +37,7 @@ namespace EManagersLib {
             } catch (Exception e) {
                 UnityEngine.Debug.LogException(e);
             }
+            ESettings.LoadSettings();
             EUtils.CheckIncompatibleMods();
             HarmonyHelper.DoOnHarmonyReady(() => EUtils.EnablePatches());
         }
@@ -48,6 +46,7 @@ namespace EManagersLib {
             if (HarmonyHelper.IsHarmonyInstalled) {
                 EUtils.DisablePatches();
             }
+            ESettings.SaveSettings();
         }
 
         public void OnSettingsUI(UIHelperBase helper) {
@@ -60,6 +59,21 @@ namespace EManagersLib {
             msg.text = "Extended Managers Library is a mod to extend the default Cities Skylines Framework.\n" +
                        "Currently, only prop limit can be extended beyond 65k\n\n" +
                        "This mod also includes a statistic panel, which you can bring out by pressing the following hotkey. This statistics panel is updated every 5 seconds\n";
+            UISlider slider = helperPanel.AddSlider("Adjust Maximum Outside Connections", 4, 64, 2, ESettings.m_maxOutsideConnection, (value) => {
+                if (ESettings.m_maxOutsideConnection != value) {
+                    ESettings.m_maxOutsideConnection = (int)value;
+                    ESettings.SaveSettings();
+                }
+            }) as UISlider;
+            slider.width = 400f;
+            helperPanel.AddSpace((int)slider.height);
+            helperPanel.AddCheckbox("Enable Electrified Road", ESettings.m_electrifiedRoad, (isChecked) => {
+                if(ESettings.m_electrifiedRoad != isChecked) {
+                    ESettings.m_electrifiedRoad = isChecked;
+                    ESettings.SaveSettings();
+                }
+            });
+            helperPanel.AddSpace(20);
             root.gameObject.AddComponent<EKeyBinding>();
         }
 
